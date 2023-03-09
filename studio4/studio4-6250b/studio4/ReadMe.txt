463f52eef4e43a86b4d999de97826f344653f347
Ermu Lu <99313780+kelly7337@users.noreply.github.com>
2023-03-09 14:44:02 -0600

    Add files via upload

---
+++ studio4/studio4-6250b/studio4/ReadMe.txt
@@ -0,0 +1,85 @@
+1. Ermu Lu
+
+2. test[0][0][0] = 0
+test[0][0][1] = 0
+test[0][0][2] = 0
+test[0][0][3] = 0
+test[0][0][4] = 0
+test[0][1][0] = 0
+test[0][1][1] = 0
+test[0][1][2] = 0
+test[0][1][3] = 0
+test[0][1][4] = 0
+test[0][2][0] = 0
+test[0][2][1] = 0
+test[0][2][2] = 0
+test[0][2][3] = 0
+test[0][2][4] = 0
+test[1][0][0] = 0
+test[1][0][1] = 0
+test[1][0][2] = 0
+test[1][0][3] = 0
+test[1][0][4] = 0
+test[1][1][0] = 0
+test[1][1][1] = 1
+test[1][1][2] = 2
+test[1][1][3] = 3
+test[1][1][4] = 4
+test[1][2][0] = 0
+test[1][2][1] = 2
+test[1][2][2] = 4
+test[1][2][3] = 6
+test[1][2][4] = 8
+
+3. E:\VS_code\504\studio4\studio4-6250b\studio4\console3\x64\Debug\console3.exeErmuLu
+   yes.
+
+4. Ermu
+Lu
+yes.
+
+5. (1)0 E:\VS_code\504\studio4\studio4-6250b\studio4\console3\x64\Debug\console3.exe
+      1 ErmuLu
+      And the command output is 0.
+   (2)0 E:\VS_code\504\studio4\studio4-6250b\studio4\console3\x64\Debug\console3.exe
+      1 ErmuLu
+      2 aaa
+      And the command output is 0.
+   (3)0 E:\VS_code\504\studio4\studio4-6250b\studio4\console3\x64\Debug\console3.exe
+      1 ErmuLu
+      2 aaa
+      3 bbb
+      And the command output is 0.
+
+7. test[0][0][0] = 0address:000000B81736F6C0
+test[0][0][1] = 0address:000000B81736F6C4
+test[0][0][2] = 0address:000000B81736F6C8
+test[0][0][3] = 0address:000000B81736F6CC
+test[0][0][4] = 0address:000000B81736F6D0
+test[0][1][0] = 0address:000000B81736F6D4
+test[0][1][1] = 0address:000000B81736F6D8
+test[0][1][2] = 0address:000000B81736F6DC
+test[0][1][3] = 0address:000000B81736F6E0
+test[0][1][4] = 0address:000000B81736F6E4
+test[0][2][0] = 0address:000000B81736F6E8
+test[0][2][1] = 0address:000000B81736F6EC
+test[0][2][2] = 0address:000000B81736F6F0
+test[0][2][3] = 0address:000000B81736F6F4
+test[0][2][4] = 0address:000000B81736F6F8
+test[1][0][0] = 0address:000000B81736F6FC
+test[1][0][1] = 0address:000000B81736F700
+test[1][0][2] = 0address:000000B81736F704
+test[1][0][3] = 0address:000000B81736F708
+test[1][0][4] = 0address:000000B81736F70C
+test[1][1][0] = 0address:000000B81736F710
+test[1][1][1] = 1address:000000B81736F714
+test[1][1][2] = 2address:000000B81736F718
+test[1][1][3] = 3address:000000B81736F71C
+test[1][1][4] = 4address:000000B81736F720
+test[1][2][0] = 0address:000000B81736F724
+test[1][2][1] = 2address:000000B81736F728
+test[1][2][2] = 4address:000000B81736F72C
+test[1][2][3] = 6address:000000B81736F730
+test[1][2][4] = 8address:000000B81736F734
+
+They are 4 unit apart.
---
+++ studio4/studio4-6250b/studio4/console3/console3/console3.cpp
@@ -0,0 +1,101 @@
+﻿// console3.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include<sstream>
+using namespace std;
+
+//int main()
+//{
+//    int test[2][3][5] = {
+//                            {
+//                                {0, 0, 0, 0, 0},
+//                                {0, 0, 0, 0, 0},
+//                                {0, 0, 0, 0, 0}
+//                            },
+//                            {
+//                                {0, 0, 0, 0, 0},
+//                                {0, 0, 0, 0, 0},
+//                                {0, 0, 0, 0, 0}
+//                            }
+//    };
+//    for (int i = 0; i < 2; ++i) {
+//        for (int j = 0; j < 3; ++j) {
+//            for (int k = 0; k < 5; ++k) {
+//                test[i][j][k] = i * j * k;
+//            }
+//        }
+//    }
+//    for (int i = 0; i < 2; ++i) {
+//        for (int j = 0; j < 3; ++j) {
+//            for (int k = 0; k < 5; ++k) {
+//                cout << "test[" << i << "][" << j << "][" << k << "] = " << test[i][j][k] << endl;
+//            }
+//        }
+//    }
+//
+//}
+
+//int main(int argc, char* argv[]) {
+//	vector<string> s(argc);
+//	for (int i = 0; i < argc; ++i) {
+//		s.push_back(argv[i]);
+//	}
+//	for (auto it : s)
+//		cout << it;
+//	cout << endl;
+//	
+//}
+
+//int main() {
+//    string str = "Ermu Lu";
+//    istringstream is(str);
+//    string s;
+//    while (is >> s)
+//        cout << s << endl;
+//    
+//}
+
+//int main(int argc, char* argv[])
+//{
+//
+//    for (int count{ 0 }; count < argc; ++count)
+//    {
+//        std::cout << count << ' ' << argv[count] << '\n';
+//    }
+//
+//    return 0;
+//}
+
+int main()
+{
+    int test[2][3][5] = {
+                            {
+                                {0, 0, 0, 0, 0},
+                                {0, 0, 0, 0, 0},
+                                {0, 0, 0, 0, 0}
+                            },
+                            {
+                                {0, 0, 0, 0, 0},
+                                {0, 0, 0, 0, 0},
+                                {0, 0, 0, 0, 0}
+                            }
+    };
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            for (int k = 0; k < 5; ++k) {
+                test[i][j][k] = i * j * k;
+            }
+        }
+    }
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            for (int k = 0; k < 5; ++k) {
+                cout << "test[" << i << "][" << j << "][" << k << "] = " << test[i][j][k] << "address:" << &test[i][j][k] << endl;
+            }
+        }
+    }
+
+}
